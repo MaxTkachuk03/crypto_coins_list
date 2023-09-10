7ca53342ad01b9df5493db9fb3407351a860267a
MaxTkachuk03 <max200325622@gmail.com>
2023-09-10 22:13:39 +0300

    fifth commit(new screen, routes,asserts,arguments)

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
-//! lesson 4 ended
+//! lesson 5 ended
 
 void main() {
   runApp(const CryptoCurrenciesListApp());
@@ -24,14 +26,16 @@ class CryptoCurrenciesListApp extends StatelessWidget {
         ),
         dividerColor: Colors.white24,
         appBarTheme: const AppBarTheme(
-          titleTextStyle: TextStyle(
-            color: Colors.white,
-            fontSize: 20.0,
-            fontWeight: FontWeight.w700,
-          ),
-          backgroundColor: Color.fromARGB(255, 31, 31, 31),
-          surfaceTintColor: Color.fromARGB(255, 31, 31, 31),
-        ),
+            titleTextStyle: TextStyle(
+              color: Colors.white,
+              fontSize: 20.0,
+              fontWeight: FontWeight.w700,
+            ),
+            backgroundColor: Color.fromARGB(255, 41, 41, 41),
+            surfaceTintColor: Color.fromARGB(255, 150, 150, 150),
+            iconTheme: IconThemeData(
+              color: Colors.white,
+            )),
         textTheme: TextTheme(
           bodyMedium: const TextStyle(
             color: Colors.white,
@@ -45,21 +49,31 @@ class CryptoCurrenciesListApp extends StatelessWidget {
           ),
         ),
       ),
-      home: const MyHomePage(),
+      //! Перший метод
+      routes: {
+        '/': (context) => const CryptoListScreen(),
+        '/coin': (context) => const CryptoCoinScreen(),
+      },
+      //! Другий метод
+      // routes: {
+      //   '/coins-list' : (context) => const CryptoListScreen(),
+      //   '/coin' : (context) => const CryptoCoinScreen(),
+      // },
+      // initialRoute: "/coins-list",
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({
+class CryptoListScreen extends StatefulWidget {
+  const CryptoListScreen({
     super.key,
   });
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<CryptoListScreen> createState() => _CryptoListScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _CryptoListScreenState extends State<CryptoListScreen> {
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
@@ -75,21 +89,78 @@ class _MyHomePageState extends State<MyHomePage> {
             color: theme.dividerColor,
           );
         },
-        itemBuilder: (context, index) => ListTile(
-          leading: SvgPicture.asset(
-            'assets/svg/bitcoin_logo.svg',
-            height: 30.0,
-            width: 30.0,
-          ),
-          title: Text(
-            "Bitcoin",
-            style: theme.textTheme.bodyMedium,
-          ),
-          subtitle: Text(
-            "20000\$",
-            style: theme.textTheme.labelSmall,
-          ),
-          trailing: Icon(Icons.arrow_forward_ios),
+        itemBuilder: (context, index) {
+          const coinName = "Bitcoin";
+          return ListTile(
+            leading: SvgPicture.asset(
+              'assets/svg/bitcoin_logo.svg',
+              height: 30.0,
+              width: 30.0,
+            ),
+            title: Text(
+              coinName,
+              style: theme.textTheme.bodyMedium,
+            ),
+            subtitle: Text(
+              "20000\$",
+              style: theme.textTheme.labelSmall,
+            ),
+            trailing: const Icon(Icons.arrow_forward_ios),
+            onTap: () {
+              Navigator.of(context).pushNamed(
+                "/coin",
+                arguments: coinName,
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
+
+class CryptoCoinScreen extends StatefulWidget {
+  const CryptoCoinScreen({super.key});
+
+  @override
+  State<CryptoCoinScreen> createState() => _CryptoCoinScreenState();
+}
+
+class _CryptoCoinScreenState extends State<CryptoCoinScreen> {
+  String? coinName;
+
+  @override
+  void didChangeDependencies() {
+    final args = ModalRoute.of(context)?.settings.arguments;
+    //! Перший метод(кращий, ефективніший)
+    assert(args!= null && args is String, 'You must provide String args');
+    coinName = args as String;
+    //! Другий метод
+    // if (args == null) {
+    //   debugPrint('You must provide args');
+    //   return;
+    // }
+    // if (args is! String) {
+    //   debugPrint('You must provide String args');
+    //   return;
+    // }
+    // coinName = args;
+    setState(() {});
+    super.didChangeDependencies();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(coinName ?? "..."),
+        leading: IconButton(
+          enableFeedback: true,
+          icon: const Icon(Icons.arrow_back_ios),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
         ),
       ),
     );
